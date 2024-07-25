416e343f766d13a751c1ff40f61a18ae53e42818
olenafierbiei <ferbeylena@gmail.com>
2024-07-25 19:41:02 +0200

    Add files via upload

---
+++ mein/java.js
@@ -0,0 +1,15 @@
+//write yuor code here
+var x = 3;
+var y = 3 * x;
+print(y)
+
+function square (x) {
+    return x * x
+}
+ y= square(5);
+ //print (y);
+ print(square(4));
+
+ var img = new SimpleImage("chapel.png");
+ print(img);
+ 
